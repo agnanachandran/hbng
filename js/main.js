e69d9c3f221aee201e1b7d69f2036ef9b741f998
agnanachandran <anojhgnanachandran@gmail.com>
2013-08-03 01:13:09 -0400

    Shortened js and removed more useless things

--- js/main.js
+++ js/main.js
@@ -1,65 +1,41 @@
-var words = {};
+var words = {
+    Noun: ["apples", "babies", "balls", "beds", "bears", "boys", "bells", "birds", "brothers", "boats", "giants", "dinosaurs", "cakes", "cars", "cats", "children", "corn", "chairs", "chickens", "cows", "dogs", "wind", "dolls", "frogs", "ducks", "eggs", "eyes", "snails", "waves", "lizards", "feet", "clouds", "fish", "trains", "flowers", "pets", "books", "girls", "snakes", "grass", "pies", "hands", "pizzas", "oranges", "bikes", "horses", "houses", "kittens", "legs", "letters", "ants", "men", "tomatoes", "money", "teeth", "mice", "friends", "spiders", "pigs", "rabbits", "rain", "rings", "clocks", "fairies", "planes", "songs", "sheep", "shoes", "sisters", "trees", "plants", "trucks", "sticks", "sun", "toys", "things"],
+    Verb:["creeping", "crawling", "walking", "running", "jumping", "skipping", "hoping", "slithering", "climbing", "diging", "squirming", "flying", "sitting", "stalking", "stomping", "tiptoeing", "galloping", "blowing", "dancing", "gliding", "swimming", "washing", "playing", "throwing", "drinking", "eating", "chewing", "singing", "shouting", "growling", "barking", "buzzing", "laughing", "smiling", "crying", "mooing", "quacking", "talking", "yelling", "screaming", "screeching", "squawking", "squealing", "glowing", " listening", "painting", "looking", "reading", "knitting", " sleeping", "drawing", "shining", "watching", "kicking", "diving", "finding", "building", "working", "exploring", "shopping", "cleaning", "catching", "shake"],
+    Adjective:["average", "big", "colossal", "fat", "giant", "gigantic", "great", "huge", "immense", "large", "little", "long", "mammoth", "massive", "miniature", "petite", "puny", "short", "small", "tall", "tiny", "boiling", "breezy", "broken", "bumpy", "chilly", "cold", "cool", "creepy", "crooked", "cuddly", "curly", "damaged", "damp", "dirty", "dry", "dusty", "filthy", "flaky", "fluffy", "wet", "broad", "chubby", "crooked", "curved", "deep", "flat", "high", "hollow", "low", "narrow", "round", "shallow", "skinny", "square", "steep", "straight", "wide", "ancient", "brief", "early", "fast", "late", "long", "modern", "old", "old-fashioned", "quick", "rapid", "short", "slow", "swift", "young", "abundant", "empty", "few", "heavy", "light", "many", "numerous", "sound", "cooing", "deafening", "faint"]
+};
 
-words.Noun = new Array("apples", "babies", "balls", "beds", "bears", "boys", "bells", "birds", "brothers", "boats", "giants", "dinosaurs", "cakes", "cars", "cats", "children", "corn", "chairs", "chickens", "cows", "dogs", "wind", "dolls", "frogs", "ducks", "eggs", "eyes", "snails", "waves", "lizards", "feet", "clouds", "fish", "trains", "flowers", "pets", "books", "girls", "snakes", "grass", "pies", "hands", "pizzas", "oranges", "bikes", "horses", "houses", "kittens", "legs", "letters", "ants", "men", "tomatoes", "money", "teeth", "mice", "friends", "spiders", "pigs", "rabbits", "rain", "rings", "clocks", "fairies", "planes", "songs", "sheep", "shoes", "sisters", "trees", "plants", "trucks", "sticks", "sun", "toys", "things");
+$(function() {
 
-words.Verb = new Array("creeping", "crawling", "walking", "running", "jumping", "skipping", "hoping", "slithering", "climbing", "diging", "squirming", "flying", "sitting", "stalking", "stomping", "tiptoeing", "galloping", "blowing", "dancing", "gliding", "swimming", "washing", "playing", "throwing", "drinking", "eating", "chewing", "singing", "shouting", "growling", "barking", "buzzing", "laughing", "smiling", "crying", "mooing", "quacking", "talking", "yelling", "screaming", "screeching", "squawking", "squealing", "glowing", " listening", "painting", "looking", "reading", "knitting", " sleeping", "drawing", "shining", "watching", "kicking", "diving", "finding", "building", "working", "exploring", "shopping", "cleaning", "catching", "shake");
+    var picks = {
+        $one:$('#starting-one'),
+        $two:$('#starting-two'),
+        $three:$('#starting-three')
+    };
+    
+    var numbers = ['one', 'two', 'three'];
 
-words.Adjective = new Array("average", "big", "colossal", "fat", "giant", "gigantic", "great", "huge", "immense", "large", "little", "long", "mammoth", "massive", "miniature", "petite", "puny", "short", "small", "tall", "tiny", "boiling", "breezy", "broken", "bumpy", "chilly", "cold", "cool", "creepy", "crooked", "cuddly", "curly", "damaged", "damp", "dirty", "dry", "dusty", "filthy", "flaky", "fluffy", "wet", "broad", "chubby", "crooked", "curved", "deep", "flat", "high", "hollow", "low", "narrow", "round", "shallow", "skinny", "square", "steep", "straight", "wide", "ancient", "brief", "early", "fast", "late", "long", "modern", "old", "old-fashioned", "quick", "rapid", "short", "slow", "swift", "young", "abundant", "empty", "few", "heavy", "light", "many", "numerous", "sound", "cooing", "deafening", "faint");
-
-var $pickOne = null;
-var $pickTwo = null;
-var $pickThree = null;
-
-$(document).ready(function() {
-
-    function onClickity() {
-        $pickOne = $('.option-one').find('.focus');
-        $pickTwo = $('.option-two').find('.focus');
-        $pickThree = $('.option-three').find('.focus');
-    }
-
-    onClickity(); 
-
-    $(".button").click(function() {
-        if (($(this).parent().hasClass('option-one'))) {
-            $('.option-one').find('.focus').removeClass('focus active');
-        }
-        else if (($(this).parent().hasClass('option-two'))) {
-            $('.option-two').find('.focus').removeClass('focus active');
-        }
-        else if (($(this).parent().hasClass('option-three'))) {
-            $('.option-three').find('.focus').removeClass('focus active');
+    $('.button').click(function() {
+        for (i = 0; i < numbers.length; i++) {
+            if (($(this).parent().hasClass('option-' + numbers[i]))) {
+                picks['$'+numbers[i]].removeClass('focus active');
+                picks['$'+numbers[i]] = $(this);
+            }
         }
         $(this).addClass('focus active');
-        onClickity();
     });
 
-    var changeWords = function(wordOne, wordTwo, wordThree) {
-        $('.word-one').text(wordOne);
-        $('.word-two').text(wordTwo);
-        $('.word-three').text(wordThree);
+    var getRandomNumber = function(max) {
+        return Math.floor((Math.random()*max));
     }
 
-    $(".generate").click(function() {
-        var strOne = $('.option-one').find('.focus').html();
-        var strTwo = $('.option-two').find('.focus').html();
-        var strThree = $('.option-three').find('.focus').html();
-        
-        var randOne = Math.floor((Math.random()*words[strOne].length));
-        var randTwo = Math.floor((Math.random()*words[strTwo].length));
-        var randThree = Math.floor((Math.random()*words[strThree].length));
-
-        var wordOne = words[strOne][randOne];
-        var wordTwo = words[strTwo][randTwo];
-        var wordThree = words[strThree][randThree];
-        // Implement this properly
-        var realOne = wordOne;
-        var realTwo = wordTwo;
-        var realThree = wordThree;
-        }
-
-        $('.band-text').fadeIn(500, function() {
-            $(this).text('Your hipster band name is: ' + realOne + " " + realTwo + " " + realThree);
-        });
+    $('.generate').click(function() {
+        var strings = [picks.$one.html(), picks.$two.html(), picks.$three.html()];
+        var randoms = [getRandomNumber(words[strings[0]].length),
+                       getRandomNumber(words[strings[1]].length),
+                       getRandomNumber(words[strings[2]].length)];
+        var names = [words[strings[0]][randoms[0]],
+                     words[strings[1]][randoms[1]],
+                     words[strings[2]][randoms[2]]];
+        $('.band-text').fadeIn().text('Your hipster band name is: ' + names[0] + ' ' + names[1] + ' ' + names[2]);
     });
 });
