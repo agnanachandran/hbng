4b8868a81d64718d212475166d9d465e68d4ebb2
Anojh Gnanachandran <anojhgnanachandran@gmail.com>
2014-03-05 21:22:16 -0500

    Fix typos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var words = {
     Noun: ["Apples", "Babies", "Balls", "Beds", "Bears", "Boys", "Bells", "Birds", "Brothers", "Boats", "Giants", "Dinosaurs", "Cakes", "Cars", "Cats", "Children", "Corn", "Chairs", "Chickens", "Cows", "Dogs", "Wind", "Dolls", "Frogs", "Ducks", "Eggs", "Eyes", "Snails", "Waves", "Lizards", "Feet", "Clouds", "Fish", "Trains", "Flowers", "Pets", "Books", "Girls", "Snakes", "Grass", "Pies", "Hands", "Pizzas", "Oranges", "Bikes", "Horses", "Houses", "Kittens", "Legs", "Letters", "Ants", "Men", "Tomatoes", "Money", "Teeth", "Mice", "Friends", "Spiders", "Pigs", "Rabbits", "Rain", "Rings", "Clocks", "Fairies", "Planes", "Songs", "Sheep", "Shoes", "Sisters", "Trees", "Plants", "Trucks", "Sticks", "Sun", "Toys", "Things"],
-    Verb:["Creeping", "Crawling", "Walking", "Running", "Jumping", "Skipping", "Hoping", "Slithering", "Climbing", "Diging", "Squirming", "Flying", "Sitting", "Stalking", "Stomping", "Tiptoeing", "Galloping", "Blowing", "Dancing", "Gliding", "Swimming", "Washing", "Playing", "Throwing", "Drinking", "Eating", "Chewing", "Singing", "Shouting", "Growling", "Barking", "Buzzing", "Laughing", "Smiling", "Crying", "Mooing", "Quacking", "Talking", "Yelling", "Screaming", "Screeching", "Squawking", "Squealing", "Glowing", " Listening", "Painting", "Looking", "Reading", "Knitting", " Sleeping", "Drawing", "Shining", "Watching", "Kicking", "Diving", "Finding", "Building", "Working", "Exploring", "Shopping", "Cleaning", "Catching", "Shake"],
+    Verb:["Creeping", "Crawling", "Walking", "Running", "Jumping", "Skipping", "Hoping", "Slithering", "Climbing", "Digging", "Squirming", "Flying", "Sitting", "Stalking", "Stomping", "Tiptoeing", "Galloping", "Blowing", "Dancing", "Gliding", "Swimming", "Washing", "Playing", "Throwing", "Drinking", "Eating", "Chewing", "Singing", "Shouting", "Growling", "Barking", "Buzzing", "Laughing", "Smiling", "Crying", "Mooing", "Quacking", "Talking", "Yelling", "Screaming", "Screeching", "Squawking", "Squealing", "Glowing", "Listening", "Painting", "Looking", "Reading", "Knitting", "Sleeping", "Drawing", "Shining", "Watching", "Kicking", "Diving", "Finding", "Building", "Working", "Exploring", "Shopping", "Cleaning", "Catching", "Shaking"],
     Adjective:["average", "big", "Colossal", "Fat", "Giant", "Gigantic", "Great", "Huge", "Immense", "Large", "Little", "Long", "Mammoth", "Massive", "Miniature", "Petite", "Puny", "Short", "Small", "Tall", "Tiny", "Boiling", "Breezy", "Broken", "Bumpy", "Chilly", "Cold", "Cool", "Creepy", "Crooked", "Cuddly", "Curly", "Damaged", "Damp", "Dirty", "Dry", "Dusty", "Filthy", "Flaky", "Fluffy", "Wet", "Broad", "Chubby", "Crooked", "Curved", "Deep", "Flat", "High", "Hollow", "Low", "Narrow", "Round", "Shallow", "Skinny", "Square", "Steep", "Straight", "Wide", "Ancient", "Brief", "Early", "Fast", "Late", "Long", "Modern", "Old", "Old-fashioned", "Quick", "Rapid", "Short", "Slow", "Swift", "Young", "Abundant", "Empty", "Few", "Heavy", "Light", "Many", "Numerous", "Sound", "Cooing", "Deafening", "Faint"]
 };
 
@@ -14,7 +14,7 @@ $(function() {
     var names = [];
 
     $('.button').click(function() {
-        for (i = 0; i < numbers.length; i++) {
+        for (var i = 0; i < numbers.length; i++) {
             if (($(this).parent().hasClass('option-' + numbers[i]))) {
                 picks['$'+numbers[i]].removeClass('focus active');
                 picks['$'+numbers[i]] = $(this);
@@ -28,7 +28,7 @@ $(function() {
     }
 
     $('.generate').click(function() {
-        for(i = 0; i < numbers.length; i++) {
+        for(var i = 0; i < numbers.length; i++) {
             choiceType = picks['$'+numbers[i]].html();
             randomNumber = getRandomNumber(words[choiceType].length);
             names[i] = words[choiceType][randomNumber];
